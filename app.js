63ba2384532b2ab9ed2f7c06e99a7d627c7def45
Everton Misiagia <eamissiagia@gmail.com>
2025-01-17 19:58:10 -0300

    Completando a função do EventListener para mostrar tarefas adicionadas na tela.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const inputItem = document.getElementById("input-item"); // Encontra no HTML um elemento específico a partir do seu ID.
 const botaoAdd = document.getElementById("adicionar-item");
+const toDoList = document.getElementById("lista-de-compras");
 
-botaoAdd.addEventListener("click", (evento) => {
+let contador = 0; // Faz o controle do número de itens criados.
+
+botaoAdd.addEventListener("click",  (evento) => {
     evento.preventDefault(); // Impede que o comportamento padrão do evento aconteça. Por exemplo, impede que um link abra uma nova página.
     console.log(inputItem.value); // A partir do click capturado, envia pro console o valor capturado do form.
 
@@ -10,7 +13,21 @@ botaoAdd.addEventListener("click", (evento) => {
         return // O return interrompe a execução e retorna ou não um valor chamado pela função. Return só é usado em functions, e o break só em loops.
     }
 
-    const itemDaLista = document.createElement("li");
-    const containerItemDalista = document.createElement("div")
-    containerItemDalista.classList.add("main__form__list__item")
+    const itemDaLista = document.createElement("li"); // Cria um novo item de lista
+    const containerItemDalista = document.createElement("div"); // Cria uma div e à atribui a essa variável.
+    containerItemDalista.classList.add("list__item"); // Adiciona uma classe para a div criada acima.
+
+    const inputCheckbox = document.createElement("input"); // Acessa o documento e cria um elemento input.
+    inputCheckbox.type = "checkbox"; // Adiciona a esse elemento um tipo específico.
+    inputCheckbox.id = "checkbox-" + contador++; // Modificao ID do elemento a partir do contador.
+
+    const listTarefa = document.createElement("p"); // Acessa o documento e cria um elemento <p>.
+    listTarefa.innerText = inputItem.value; // Atribui a esse elemento o valor recuperado da variável da barra de digitação.
+
+    containerItemDalista.appendChild(inputCheckbox); // Adiciona o checkbox dentro da div criada.
+    containerItemDalista.appendChild(listTarefa); // Adiciona o parágrafo dentro da div criada.
+
+    itemDaLista.appendChild(containerItemDalista); // Adiciona a div criada (e seus filhos) dentro do elemento "li" criado.
+
+    toDoList.appendChild(itemDaLista); // Adiciona o item de lista com todos os filhos à lista principal.
 });
